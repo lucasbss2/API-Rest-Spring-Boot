01feeeaa16556935ca89c7ef5d4961af169153cc
Lucas Borges <106633015+lucasbss2@users.noreply.github.com>
2025-09-06 01:21:45 -0300

    Update SecurityConfig.java
    Adição de /error

--- src/main/java/com/example/API_Rest/security/SecurityConfig.java
+++ src/main/java/com/example/API_Rest/security/SecurityConfig.java
@@ -44,7 +44,7 @@ public class SecurityConfig {
         return http
                 .csrf(csrf -> csrf.disable())
                 .authorizeHttpRequests(auth -> auth
-                        .requestMatchers("/","/auth/**", "/swagger-ui/**", "/v3/api-docs/**")
+                        .requestMatchers("/","/auth/**", "/swagger-ui/**", "/v3/api-docs/**", "/error")
                         .permitAll()
                         .anyRequest()
                         .authenticated())
